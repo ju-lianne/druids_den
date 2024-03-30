30900b167adaf06e2a8c09e88323f528057a6a38
Julianne Duhaze <jduhaze@protonmail.com>
2024-03-30 19:59:15 +0100

    Intialization

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import './App.css';
+
+import Homepage from "./pages/Home/Home.page";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Homepage />}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
