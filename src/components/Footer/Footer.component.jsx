c463ef8abc3078593a601fc6e0f525346087869b
Julianne Duhaze <jduhaze@protonmail.com>
2024-03-31 10:54:55 +0200

    feat: Finalized footer

--- src/components/Footer/Footer.component.jsx
+++ src/components/Footer/Footer.component.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
   return (
     <div className='footer'>
-        <h1>Tit Ouistiti</h1>
+      <Link className='link' to='/credits'>Crédits</Link>
     </div>
   )
 }
