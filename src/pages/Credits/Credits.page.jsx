27b4d705fd235dcc360be34394fb70e61c948416
Julianne Duhaze <jduhaze@protonmail.com>
2024-03-31 11:52:54 +0200

    feat: Created credits page

---
+++ src/pages/Credits/Credits.page.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../components/Header/Header.component';
+import Footer from '../../components/Footer/Footer.component';
+import CardsContainer from '../../components/CardsContainer/CardsContainer.component';
+
+export default function Credits() {
+  return (
+    <div className='page'>
+      <Header />
+      <CardsContainer />
+      <Footer />
+    </div>
+  )
+}
