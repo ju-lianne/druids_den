ed8a6841deb06b35960207a10ebc2653c0d19c44
Julianne Duhaze <jduhaze@protonmail.com>
2024-03-31 16:55:50 +0200

    feat: Modified Credits page

--- src/pages/Credits/Credits.page.jsx
+++ src/pages/Credits/Credits.page.jsx
@@ -10,8 +10,8 @@ export default function Credits() {
     <div className='page credits'>
       <Header />
       <div className='page-body'>
-        <h1>Sources des images utilisées</h1>
-        <p>Cliquer sur l'image pour accéder au portfolio de l'artiste sur Unsplash.</p>
+        <h1>Sources des images utilisées sur le site</h1>
+        <p>Cliquer sur l'image pour accéder au portfolio de l'artiste sur Unsplash.com.</p>
       <CardsContainer data={data} />
       </div>
       <Footer />
